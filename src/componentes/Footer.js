63155aa7b5c63a90289ecac2595f3b398d5314e8
Joaquín Villanueva <villanuevajoaquin94@gmail.com>
2020-11-13 17:31:38 -0300

    Iconos de mev y pjn agregados a los links

--- src/componentes/Footer.js
+++ src/componentes/Footer.js
@@ -1,29 +1,22 @@
 import React from "react";
-import PoderJudicial from "./icon.svg";
+//ings
+import PJN from "./pjn.png";
+import MEV from "./mev.png";
 
 export default function Footer() {
   return (
     <div className="footer">
-      <a
-        href="https://www.linkedin.com/in/joaqu%C3%ADn-villanueva-3b5857173/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="footer-link"
-      >
-        <i className="fab fa-linkedin-in"></i>
-      </a>
       <a
         href="https://mev.scba.gov.ar/loguin.asp"
         target="_blank"
         rel="noopener noreferrer"
       >
         <img
-          src={PoderJudicial}
+          src={MEV}
           alt=""
           style={{
-            width: "16px",
-            height: "16px",
-            margin: "0rem .5rem 0rem .5rem",
+            width: "50px",
+            height: "50px",
           }}
         />
       </a>
@@ -33,12 +26,11 @@ export default function Footer() {
         rel="noopener noreferrer"
       >
         <img
-          src={PoderJudicial}
+          src={PJN}
           alt=""
           style={{
-            width: "16px",
-            height: "16px",
-            margin: "0rem .5rem 0rem .5rem",
+            width: "50px",
+            height: "50px",
           }}
         />
       </a>
